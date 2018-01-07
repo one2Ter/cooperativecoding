3ada4b4c2f309778ce298f94acd16932c89d6454
lixinyu1024 <lixinyu1024@gmail.com>
2018-01-07 15:07:39 +0800

    Fixed messy code issue in mysql database

--- src/main/java/com/lixinyu/cooperativecoding/Application.java
+++ src/main/java/com/lixinyu/cooperativecoding/Application.java
@@ -1,7 +1,11 @@
 package com.lixinyu.cooperativecoding;
 
+import com.lixinyu.cooperativecoding.Entries.User;
+import com.lixinyu.cooperativecoding.Entries.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -11,4 +15,12 @@ public class Application {
 		SpringApplication.run(Application.class, args
 		);
 	}
+
+    @Bean
+    public CommandLineRunner db_init(UserRepository repository){
+        //删除数据库并重建
+        return(args) -> {
+            repository.save(new User(20143461,1,"李新宇",0));
+        };
+    }
 }
