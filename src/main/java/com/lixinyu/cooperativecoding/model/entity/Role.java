a0f06c2618056762d6fb178ec97ea1228aa16e32
lixinyu1024 <lixinyu1024@gmail.com>
2018-03-11 19:39:30 +0800

    Add administrative page

--- src/main/java/com/lixinyu/cooperativecoding/model/entity/Role.java
+++ src/main/java/com/lixinyu/cooperativecoding/model/entity/Role.java
@@ -1,14 +1,23 @@
 package com.lixinyu.cooperativecoding.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
+@JsonIgnoreProperties(value = {"users"})
 public class Role {
 
     @Id
     private int role_id;
     private String role;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
 
     public Role() {
     }
@@ -32,5 +41,13 @@ public class Role {
 
     public void setRole(String role) {
         this.role = role;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
     }
 }
